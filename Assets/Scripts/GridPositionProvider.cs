9ae298074e35e8a3b177530e3d7237ca4ba4c6ce
Narek Aghekyan <naghekyan@gmail.com>
2018-05-31 19:33:04 +0400

    Implemented ball movement through grid  - drag-and-drop by mouse click  - grid snapping  - after dropping snapping

---
+++ Assets/Scripts/GridPositionProvider.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AssemblyCSharp
+{
+    public class ValidPositionProvider
+    {
+        class RangeQueryCache
+        {
+            private Vector3 m_rangeCenter;
+            private float m_rangeRadius;
+            private Vector3 m_validPosition;
+            private float m_epislon = float.Epsilon;
+
+            public  RangeQueryCache() {
+                
+            }
+
+            public void SetValidPositionForRange(Vector3 rangeCenter, float rangeRadius, Vector3 validPosition) {
+                m_rangeCenter = rangeCenter;
+                m_rangeRadius = rangeRadius;
+                m_validPosition = validPosition;
+            }
+
+            public Vector3 GetCachedValidPosition() {
+                return m_validPosition;
+            }
+
+            public bool isEqualToChachedRange(Vector3 rangeCenter, float rangeRadius) {
+                return Mathf.Abs (rangeRadius - m_rangeRadius) < m_epislon
+                    && Mathf.Abs (rangeCenter.x - m_rangeCenter.x) < m_epislon
+                    && Mathf.Abs (rangeCenter.y - m_rangeCenter.y) < m_epislon
+                    && Mathf.Abs (rangeCenter.z - m_rangeCenter.z) < m_epislon; 
+            }
+        };
+
+        private List<Vector3> m_validPositions = new List<Vector3>();
+
+        private RangeQueryCache m_rangeQueryCache = new RangeQueryCache();
+
+        public ValidPositionProvider (GameObject[] listOfHoles)
+        {
+            m_rangeQueryCache = new RangeQueryCache ();
+
+            foreach (GameObject hole in listOfHoles) {
+                Vector3 holePosition = hole.transform.position;
+                m_validPositions.Add (holePosition);
+            }
+        }
+
+        public bool IsValidPositionInRangeExists(Vector3 position, float range) {
+            if (m_rangeQueryCache.isEqualToChachedRange (position, range)) {
+                return true;
+            }
+
+            float rangeSqr = range * range;
+            foreach(Vector3 validPosition in m_validPositions) {
+                Vector3 diffVector = position - validPosition;
+                float distanceSqr = diffVector.sqrMagnitude;
+                if (distanceSqr < rangeSqr) {
+                    m_rangeQueryCache.SetValidPositionForRange (position, range, validPosition);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public Vector3 GetValidPositionInRange(Vector3 position, float range) {
+            if (m_rangeQueryCache.isEqualToChachedRange (position, range)) {
+                return m_rangeQueryCache.GetCachedValidPosition();
+            }
+
+            float rangeSqr = range * range;
+            Vector3 result = position;
+            foreach (Vector3 validPosition in m_validPositions) {
+                Vector3 diffVector = position - validPosition;
+                float distanceSqr = diffVector.sqrMagnitude;
+
+                if (distanceSqr < rangeSqr) {
+                    m_rangeQueryCache.SetValidPositionForRange (position, range, validPosition);
+                    result = validPosition;
+                }
+            }
+            return result;
+        }
+    }
+}
+
---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AssemblyCSharp;
+
+public class MoveScript : MonoBehaviour 
+{
+    public float m_snapRange = 1.0f;
+
+    enum BallColor {BlueBall, WhiteBall};
+
+    private GameObject m_grabbedBall;
+    private BallColor m_colorToMove;
+    private float m_positionLerpAlpha = 0.25f;
+    private ValidPositionProvider m_validPositionProvider;
+    private Vector3 velocity = Vector3.zero;
+    private float smoothTime = 0.1F;
+
+	void Start () 
+    {
+        m_colorToMove = BallColor.BlueBall;
+        initializeValidPositions ();
+	}
+
+    private void initializeValidPositions()
+    {
+        GameObject[] listOfHoles = GameObject.FindGameObjectsWithTag("Hole");
+        m_validPositionProvider = new ValidPositionProvider (listOfHoles);
+    }
+	
+	void Update () 
+    {
+        if (Input.GetMouseButtonDown (0)) {
+            tryToGrabBall ();
+        } else if (Input.GetMouseButtonUp (0)) {
+            if (m_grabbedBall != null) {
+                StartCoroutine(SnapToValidPosition());
+            }
+        } else if (Input.GetMouseButton (0)) {
+            moveGrabbedBall ();
+        }
+	}
+
+    IEnumerator SnapToValidPosition() {
+       while (true) {
+            Vector3 grabbedBallPosition = m_grabbedBall.transform.position;
+
+            if (m_validPositionProvider.IsValidPositionInRangeExists (grabbedBallPosition, m_snapRange)) {
+                Vector3 targetPosition = m_validPositionProvider.GetValidPositionInRange (grabbedBallPosition, m_snapRange);
+                targetPosition.z = targetPosition.z - 0.2f;
+                m_grabbedBall.transform.position = Vector3.SmoothDamp (grabbedBallPosition, targetPosition, ref velocity, smoothTime);
+
+                if (Vector3.Distance (grabbedBallPosition, targetPosition) > 0.01f) {
+                    yield return null;
+                } else {
+                   break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        ungrabBall ();
+    }
+
+    private void tryToGrabBall()
+    {
+        RaycastHit hitInfo = new RaycastHit ();
+        bool hit = Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo);
+        if (hit) {
+            GameObject hitGameObject = hitInfo.transform.gameObject;
+
+            if (hitGameObject.tag == m_colorToMove.ToString ()) {
+                m_grabbedBall = hitGameObject;
+            }
+        } 
+    }
+
+    private void ungrabBall() {
+        m_grabbedBall = null;
+    }
+
+    private void moveGrabbedBall() {
+        if (m_grabbedBall == null) {
+            return;
+        }
+            
+        moveToMouse ();
+        snapToValidPositions ();
+    }
+
+    private void moveToMouse() {
+        Vector3 mousePosition = Input.mousePosition;
+        Camera camera = Camera.main;
+        float zShiftCloserToCamera = -1.0f;
+        float zPosition = -camera.transform.position.z + zShiftCloserToCamera;
+        Vector3 targetPosition = camera.ScreenToWorldPoint (new Vector3 (mousePosition.x, mousePosition.y, zPosition));
+        Vector3 currentPosition = m_grabbedBall.transform.position;
+        m_grabbedBall.transform.position = Vector3.Lerp (currentPosition, targetPosition, m_positionLerpAlpha);
+    }
+
+    private void snapToValidPositions() {
+        Vector3 grabbedBallPosition = m_grabbedBall.transform.position;
+
+        if (m_validPositionProvider.IsValidPositionInRangeExists (grabbedBallPosition, m_snapRange)) {
+            Vector3 targetPosition = m_validPositionProvider.GetValidPositionInRange (grabbedBallPosition, m_snapRange);
+            float strongLerpFactor = 1.5f * m_positionLerpAlpha;
+            m_grabbedBall.transform.position = Vector3.Lerp (grabbedBallPosition, targetPosition, strongLerpFactor);
+        }
+    }
+}
